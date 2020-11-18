058dfc17be3c91e8dbb56f4f986723b57946c000
Jaco van Stryp <jacovanstryp@gmail.com>
2020-11-18 15:29:12 +0200

    purchase updates

--- firebase-purchase.js
+++ firebase-purchase.js
@@ -16,11 +16,12 @@ firebase.analytics();
 const db = firebase.firestore();
 let Jobs = [];
 let idIndex = 0;
+let id, developer;
+let pass = generatePassword();
+
 window.onload = function() {
-    let pass = generatePassword();
     let curDomain = window.location.href;
     let ProjectIdentification = curDomain.substr(curDomain.indexOf('#') + 1, curDomain.length);
-    let id, developer;
     let data = ProjectIdentification.split("&7@!");
     id = data[0];
     developer = data[1];
@@ -29,75 +30,6 @@ window.onload = function() {
         userData.get().then(function(doc) {
             if (doc.exists) {
                 Jobs = doc.get("object");
-                for (var i = 0; i < Jobs.length; i++) {
-                    if (Jobs[i].ProjectID == id) {
-                        alert("Set Up Domain - " + Jobs[i].Domain)
-                        idIndex = i;
-                        window.open("https://za.godaddy.com/domainsearch/find?checkAvail=1&domainToCheck=" + Jobs[i].Domain);
-                        let c = confirm("I've Purchased The Domain")
-                        while (c != true) {
-
-                        }
-                        c = false;
-                        window.open("https://dcc.godaddy.com/manage/dns?domainName=" + Jobs[i].Domain)
-                        c = confirm("I've Changed The A Record To 50.87.177.72")
-                        while (c != true) {
-
-                        }
-                        c = false;
-                        window.open("https://my.bluehost.com/cgi/hosting/assign/" + Jobs[i].Domain)
-                        c = confirm("I've Assigned The Domain To BlueHost")
-                        while (c != true) {
-
-                        }
-                        c = false;
-                        window.open("https://dcc.godaddy.com/manage/dns?domainName=" + Jobs[i].Domain)
-                        c = confirm("I've changed the name servers to NS1.BLUEHOST.COM and NS2.BLUEHOST.COM")
-                        while (c != true) {
-
-                        }
-                        c = false;
-                        window.open("https://my.bluehost.com/hosting/app/#/create/wordpress")
-                        c = confirm("I've Created The Website")
-                        while (c != true) {
-
-                        }
-                        c = false;
-                        sendMail(Jobs[i].ClientName, "We just wanted to let you know that your website domain " + Jobs[i].Domain + " is now live on the public internet, we're still building your website and will let you know when everything is completed!", Jobs[i].ClientEmail, Jobs[i].DeveloperEmail);
-                        window.open("https://" + Jobs[i].Domain + "/wp-admin/user-new.php")
-                        c = confirm("I've Added The Following User:\nUsername: " + Jobs[i].DeveloperEmail + "\nPassword: " + pass)
-                        while (c != true) {
-
-                        }
-                        c = false;
-                        window.open("https://" + Jobs[i].Domain + "/wp-admin/options-permalink.php")
-                        c = prompt("I've Changed The Permalink to Post Name")
-                        while (c != true) {
-
-                        }
-                        c = false;
-                        window.open("https://" + Jobs[i].Domain + "/wp-admin/plugin-install.php?s=brizy&tab=search&type=term")
-                        c = prompt("I've Installed Brizy & Brizy Pro")
-                        while (c != true) {
-
-                        }
-                        c = false;
-
-                        Jobs[i].Status = "Website Work Started";
-                        db.collection('projects').doc(Jobs[i].DeveloperEmail).set({
-                                object: Jobs,
-                            }).then(function() {
-                                sendMail("Developer", "Your Domain Request Has Been Approved! To start Working, please visit this link - " + Jobs[i].Domain + "/wp-admin - username: " + Jobs[i].DeveloperEmail + " password: " + pass, Jobs[i].DeveloperEmail, "jacovanstryp@gmail.com");
-                                alert("Setup Complete!")
-                                window.close();
-
-                            })
-                            .catch(function(error) {
-                                alert("Something went Wrong")
-                            });
-                        break;
-                    }
-                }
                 stopLoading();
             } else {
                 window.location = "./index.html"
@@ -105,13 +37,68 @@ window.onload = function() {
                 stopLoading();
 
             }
-
-
-
         })
     } catch (e) {
         window.location = "./index.html"
     }
+}
+
+function moveOn() {
+    for (var i = 0; i < Jobs.length; i++) {
+        if (Jobs[i].ProjectID == id) {
+            if (Jobs[i].Status != "Domain Purchase Request Forwarded To Manager") {
+                window.location = "./index.html"
+            } else {
+                let btnValue = document.getElementById("mainbtn").innerHTML.trim();
+                console.log(btnValue)
+                if (btnValue == "Get Started") {
+                    window.open("https://za.godaddy.com/domainsearch/find?checkAvail=1&domainToCheck=" + Jobs[i].Domain);
+                    document.getElementById("mainbtn").innerHTML = "I've Purchased The Domain";
+                } else if (btnValue == "I've Purchased The Domain") {
+                    copyToClipboard("50.87.177.72");
+                    window.open("https://dcc.godaddy.com/manage/dns?domainName=" + Jobs[i].Domain)
+                    document.getElementById("mainbtn").innerHTML = "I've Changed The A Record To 50.87.177.72";
+                } else if (btnValue == "I've Changed The A Record To 50.87.177.72") {
+                    window.open("https://my.bluehost.com/cgi/hosting/assign/" + Jobs[i].Domain)
+                    document.getElementById("mainbtn").innerHTML = "I've Assigned The Domain To BlueHost";
+                } else if (btnValue == "I've Assigned The Domain To BlueHost") {
+                    document.getElementById("mainbtn").innerHTML = "I've changed the name servers to NS1.BLUEHOST.COM and NS2.BLUEHOST.COM";
+                    window.open("https://dcc.godaddy.com/manage/dns?domainName=" + Jobs[i].Domain)
+                } else if (btnValue == "I've changed the name servers to NS1.BLUEHOST.COM and NS2.BLUEHOST.COM") {
+                    window.open("https://my.bluehost.com/hosting/app/#/create/wordpress")
+                    document.getElementById("mainbtn").innerHTML = "I've Created The Website";
+                } else if (btnValue == "I've Created The Website") {
+                    sendMail(Jobs[i].ClientName, "We just wanted to let you know that your website domain " + Jobs[i].Domain + " is now live on the public internet, we're still building your website and will let you know when everything is completed!", Jobs[i].ClientEmail, Jobs[i].DeveloperEmail);
+                    copyToClipboard(pass);
+                    window.open("https://" + Jobs[i].Domain + "/wp-admin/user-new.php")
+                    document.getElementById("mainbtn").innerHTML = ("I've Added The Following User:\nUsername: " + Jobs[i].DeveloperEmail + "\nPassword: " + pass);
+                } else if (btnValue == ("I've Added The Following User:\nUsername: " + Jobs[i].DeveloperEmail + "\nPassword: " + pass)) {
+                    window.open("https://" + Jobs[i].Domain + "/wp-admin/options-permalink.php")
+                    document.getElementById("mainbtn").innerHTML = ("I've Changed The Permalink to Post Name");
+                } else if (btnValue == ("I've Changed The Permalink to Post Name")) {
+                    window.open("https://" + Jobs[i].Domain + "/wp-admin/plugin-install.php?s=brizy&tab=search&type=term")
+                    document.getElementById("mainbtn").innerHTML = ("I've Installed Brizy and Brizy Pro");
+                } else if (btnValue == ("I've Installed Brizy and Brizy Pro")) {
+                    Jobs[i].Status = "Website Work Started";
+                    db.collection('projects').doc(Jobs[i].DeveloperEmail).set({
+                            object: Jobs,
+                        }).then(function() {
+                            sendMail("Developer", "Your Domain Request Has Been Approved! To start Working, please visit this link - " + Jobs[i].Domain + "/wp-admin - username: " + Jobs[i].DeveloperEmail + " password: " + pass, Jobs[i].DeveloperEmail, "jacovanstryp@gmail.com");
+                            document.getElementById("mainbtn").innerHTML = ("Setup Complete");
+                            window.location = "./index.html";
+                        })
+                        .catch(function(error) {
+                            alert("Something went Wrong")
+                        });
+                }
+                break;
+            }
+        }
+    }
+}
+
+function copyToClipboard(text) {
+    window.prompt("Copy to clipboard: Ctrl+C, Enter", text);
 }
 
 function generatePassword() {
